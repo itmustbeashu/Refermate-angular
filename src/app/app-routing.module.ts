c7697fad1cea636c990d542d169aab83cf8fe727
itmustbeashu <ashusingh584@gmail.com>
2024-05-03 01:20:34 +0530

    fixed sidenav issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,25 +4,31 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [{
   path: 'login',
   loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule),
-  data: { isPublic: true }
+  data: { sideNav:false,bottomNav:false,header:true,footer:false }
 }, {
   path: 'dashboard',
   loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule),
+  data: { sideNav:true,bottomNav:true,header:true,footer:false }
 }, {
   path: 'home',
   loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule),
+  data: { sideNav:false,bottomNav:false,header:true,footer:false }
 }, {
   path: 'notification',
   loadChildren: () => import('./pages/notification/notification.module').then(m => m.NotificationModule),
+  data: { sideNav:true,bottomNav:true,header:true,footer:false }
 }, {
   path: 'profile',
   loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule),
+  data: { sideNav:true,bottomNav:true,header:true,footer:false }
 }, {
   path: 'referral',
   loadChildren: () => import('./pages/referral/referral.module').then(m => m.ReferralModule),
+  data: { sideNav:true,bottomNav:true,header:true,footer:false }
 }, {
   path: 'register',
   loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterModule),
+  data: { sideNav:false,bottomNav:false,header:true,footer:false }
 }, { path: '**', redirectTo: 'login' }];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd, ActivatedRouteSnapshot } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'referMateV1';
+  sideNav!: boolean; // Definite assignment assertion
+  layoutConfig: { sideNav: boolean, bottomNav: boolean, footer: boolean } = { sideNav: false, bottomNav: false, footer: false }; // Definite assignment assertion
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      // Accessing data from activated route
+      this.layoutConfig = this.getChildData(this.router.routerState.snapshot.root);
+    });
+  }
+
+  // Recursive function to traverse the route tree and find the data
+  private getChildData(route: ActivatedRouteSnapshot): { sideNav: boolean, bottomNav: boolean, footer: boolean } {
+    let sideNav = route.data['sideNav'];
+    let bottomNav = route.data['bottomNav'];
+    let footer = route.data['footer'];
+
+    if (sideNav !== undefined && bottomNav !== undefined && footer !== undefined) {
+      return { sideNav, bottomNav, footer };
+    }
+    if (route.firstChild) {
+      return this.getChildData(route.firstChild);
+    }
+    return { sideNav: false, bottomNav: false, footer: false };
+  }
+
+  get isSideNav(): boolean {
+    return this.layoutConfig.sideNav;
+  }
+  get isBottomNav(): boolean {
+    return this.layoutConfig.bottomNav;
+  }
+  get isFooter(): boolean {
+    return this.layoutConfig.footer;
+  }
+  
 }
