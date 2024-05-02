f03021a23f4089c30a59acd3ef74b45e225a5392
Ashish Singh <78902772+itmustbeashu@users.noreply.github.com>
2024-05-02 13:45:14 +0530

    Update referral.component.ts

--- src/app/pages/referral/referral.component.ts
+++ src/app/pages/referral/referral.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-referral',
   templateUrl: './referral.component.html',
-  styleUrl: './referral.component.css'
+  styleUrl: './referral.component.scss'
 })
 export class ReferralComponent {
 
