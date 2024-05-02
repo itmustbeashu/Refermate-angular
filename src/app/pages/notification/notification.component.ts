60716154658166d5e519d4065f620d6055c5aa2f
Ashish Singh <78902772+itmustbeashu@users.noreply.github.com>
2024-05-02 13:43:40 +0530

    Update notification.component.ts

--- src/app/pages/notification/notification.component.ts
+++ src/app/pages/notification/notification.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-notification',
   templateUrl: './notification.component.html',
-  styleUrl: './notification.component.css'
+  styleUrl: './notification.component.scss'
 })
 export class NotificationComponent {
 
