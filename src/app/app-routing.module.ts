7d1b530caea9b15265e0bf9c58de46ae8b8af982
Ashish Singh <78902772+itmustbeashu@users.noreply.github.com>
2024-05-02 13:36:44 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,7 +23,7 @@ const routes: Routes = [{
 }, {
   path: 'register',
   loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterModule),
-}];
+}, { path: '**', redirectTo: 'login' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
