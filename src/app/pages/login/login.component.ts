bd67c63fc60765c77d2700af56fb75604ebb3038
Ashish Singh <78902772+itmustbeashu@users.noreply.github.com>
2024-05-02 13:42:55 +0530

    Update login.component.ts

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   ngOnInit() {
