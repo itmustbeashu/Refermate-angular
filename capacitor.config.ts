f1dc96c0850cee7fa8b5c3a83179d75101a3315f
itmustbeashu <ashusingh584@gmail.com>
2024-05-08 02:26:46 +0530

    working android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'refer-mate-v1',
+  webDir: 'dist/refer-mate-v1/browser'
+};
+
+export default config;
